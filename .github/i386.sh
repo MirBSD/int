dcffb3bb9c955a9c64596af1d3ec22bea5d71e03
mirabilos <m@mirbsd.org>
2023-08-14 19:22:24 +0200

    split i386 on ±LFS ±ASan/UBSan and move to sid

--- .github/i386.sh
+++ .github/i386.sh
@@ -1,8 +1,32 @@
+dbmo_f=future=+all,-lfs
+dbmo_q=qa=+all,-canary
+dbmo_o=optimize=+all,-lto
+dbmo_h=hardening=+all
+dbmo_r=reproducible=+all
+cfx=
+cfx_asan='-fsanitize=address -fno-omit-frame-pointer -fno-common -fsanitize=pointer-compare -fsanitize=pointer-subtract -fsanitize=undefined -fsanitize=shift -fsanitize=shift-exponent -fsanitize=shift-base -fsanitize=integer-divide-by-zero -fsanitize=unreachable -fsanitize=vla-bound -fsanitize=null -fsanitize=signed-integer-overflow -fsanitize=bounds -fsanitize=bounds-strict -fsanitize=alignment -fsanitize=object-size -fsanitize=nonnull-attribute -fsanitize=returns-nonnull-attribute -fsanitize=bool -fsanitize=enum -fsanitize=vptr -fsanitize=pointer-overflow -fsanitize=builtin -fsanitize-address-use-after-scope -fstack-clash-protection'
 set -ex
+while test $# -gt 0; do
+	case $1 in
+	(lfs)
+		dbmo_f=future=+all
+		;;
+	(asan)
+		ASAN_OPTIONS=check_initialization_order=true:detect_stack_use_after_return=true:detect_invalid_pointer_pairs=2:dump_instruction_bytes=true:color=never:strict_string_checks=true:exitcode=251
+		export ASAN_OPTIONS
+		cfx="-Og $cfx_asan"
+		;;
+	(*)
+		echo >&2 "E: unknown option: $1"
+		exit 1 ;;
+	esac
+	shift
+done
+dbmo="$dbmo_f $dbmo_q $dbmo_o $dbmo_h $dbmo_r"
 apt-get update
 apt-get --purge -y dist-upgrade
 apt-get install -y bc build-essential
 : "${CC=cc}${CFLAGS=-O2}"
-eval "$(env DEB_BUILD_MAINT_OPTIONS='future=+all qa=+all,-canary optimize=+all,-lto hardening=+all reproducible=+all' dpkg-buildflags --export=sh || :)"
+eval "$(env DEB_BUILD_MAINT_OPTIONS="$dbmo" dpkg-buildflags --export=sh || :)"
 export LDFLAGS
-exec sh mkt-int.sh $CC $CPPFLAGS $CFLAGS -Wall -Wextra
+exec sh mkt-int.sh $CC $CPPFLAGS $CFLAGS -Wall -Wextra $cfx
