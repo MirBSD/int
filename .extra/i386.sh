5fc1ca61a58dd8a50c592ae3e8862bbc3667d8be
mirabilos <m@mirbsd.org>
2023-09-13 01:33:54 +0200

    more options to force treating input files as CFrustFrust when testing precisely that

--- .extra/i386.sh
+++ .extra/i386.sh
@@ -98,7 +98,7 @@ if test -n "$libc"; then
 fi
 export LDFLAGS
 case $cfx in
-(*'++'*) CC=$CXX CFLAGS=$CXXFLAGS ;;
+(*'++'*) CC=$CXX CFLAGS="$CXXFLAGS -x c++" ;;
 esac
 exec sh mkt-int.sh $CC $CPPFLAGS $cfs $cft $CFLAGS -Wall -Wextra $cfx \
     -DMBSDINT_H_WANT_PTR_IN_SIZET \
