9dc0783644f5810fdeefd2a1fe39386ff00f1c21
tg <tg@mirbsd.org>
2023-09-13 00:55:12 +0000

    split find-cl.bat off mkt-int.bat to ease option parsing and passing
    arguments to mkt-int.bat (past -cxx then -x) will be passed to CL.EXE
    and vcvarsall.bat parameters are expected with find-cl.bat

---
+++ find-cl.bat
@@ -0,0 +1,25 @@
+@echo off
+REM $MirOS: src/kern/include/find-cl.bat,v 1.1 2023/09/13 00:55:12 tg Exp $
+REM
+REM (c) 2023 mirabilos (F) CC0
+REM
+set __VSCMD_ARG_NO_LOGO=1
+set VSCMD_SKIP_SENDTELEMETRY=1
+for /F "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -property installationPath`) do (set "VSINSTALLDIR=%%i\")
+if "%1" == "" goto dohelp
+echo @echo off >do-cl.bat
+echo set __VSCMD_ARG_NO_LOGO=1 >>do-cl.bat
+echo set VSCMD_SKIP_SENDTELEMETRY=1 >>do-cl.bat
+REM echo call "%VSINSTALLDIR%Common7\Tools\VsDevCmd.bat" >>do-cl.bat
+echo call "%VSINSTALLDIR%VC\Auxiliary\Build\vcvarsall.bat" %* >>do-cl.bat
+echo cl %%* >>do-cl.bat
+echo.
+echo Ok, do-cl.bat created.
+echo.
+exit /b 0
+:dohelp
+call "%VSINSTALLDIR%VC\Auxiliary\Build\vcvarsall.bat" /help
+echo.
+echo Call this with e.g. x86 or amd64 as parameter, see above.
+echo.
+exit /b 1
--- mkt-int.bat
+++ mkt-int.bat
@@ -1,11 +1,11 @@
 @echo off
-REM $MirOS: src/kern/include/mkt-int.bat,v 1.7 2023/09/13 00:54:10 tg Exp $
+REM $MirOS: src/kern/include/mkt-int.bat,v 1.8 2023/09/13 00:55:12 tg Exp $
 REM
 REM (c) 2023 mirabilos (F) CC0
 REM
 set __VSCMD_ARG_NO_LOGO=1
 set VSCMD_SKIP_SENDTELEMETRY=1
-for /F "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -property installationPath`) do (set "VSINSTALLDIR=%%i\")
+if not exist do-cl.bat goto dohelp
 set cxx=
 set x=
 set args=%*
@@ -19,14 +19,7 @@ set x=-x
 set args=%args:~3%
 shift
 :nocross
-if "%1" == "" goto dohelp
-echo @echo off >do-cl.bat
-echo set __VSCMD_ARG_NO_LOGO=1 >>do-cl.bat
-echo set VSCMD_SKIP_SENDTELEMETRY=1 >>do-cl.bat
-REM echo call "%VSINSTALLDIR%Common7\Tools\VsDevCmd.bat" >>do-cl.bat
-echo call "%VSINSTALLDIR%VC\Auxiliary\Build\vcvarsall.bat" %args% >>do-cl.bat
-echo cl %%* >>do-cl.bat
-wsl /bin/sh mkt-int.sh %cxx% %x% "$(wslpath "%COMSPEC%")" /c do-cl.bat -DWINVER=0x0602 -D_WIN32_WINNT=0x0602 -DNTDDI_VERSION=NTDDI_WIN8 /Wall -D__STDC_WANT_SECURE_LIB__=1 /wd5045 -DMBSDINT_H_WANT_PTR_IN_SIZET -DMBSDINT_H_WANT_INT32 -DMBSDINT_H_WANT_LRG64 -DMBSDINT_H_WANT_SAFEC /nologo
+wsl /bin/sh mkt-int.sh %cxx% %x% "$(wslpath "%COMSPEC%")" /c do-cl.bat -DWINVER=0x0602 -D_WIN32_WINNT=0x0602 -DNTDDI_VERSION=NTDDI_WIN8 /Wall -D__STDC_WANT_SECURE_LIB__=1 /wd5045 -DMBSDINT_H_WANT_PTR_IN_SIZET -DMBSDINT_H_WANT_INT32 -DMBSDINT_H_WANT_LRG64 -DMBSDINT_H_WANT_SAFEC %args% /nologo
 if errorlevel 1 goto doerror
 del do-cl.bat
 exit /b 0
@@ -34,10 +27,10 @@ exit /b 0
 del do-cl.bat
 exit /b 1
 :dohelp
-call "%VSINSTALLDIR%VC\Auxiliary\Build\vcvarsall.bat" /help
 echo.
-echo Call this with e.g. x86 or amd64 as parameter, see above.
-echo Or call with something like -x x86_arm for cross compilation.
+echo Call find-cl.bat first, with e.g. x86 or amd64 as parameter.
+echo Afterwards, run mkt-int.bat with an optional first parameter
+echo of -x when cross-compiling plus optional CL.EXE arguments.
 echo Pass an initial -cxx before everything else to build as C++ not C.
 echo.
 exit /b 1
