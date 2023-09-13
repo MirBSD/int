470e9bda5f13713944e005eb5352ce9f8ffa35a6
tg <tg@mirbsd.org>
2023-09-13 00:54:11 +0000

    have the user pass -cxx to mkt-int.* if they want to test as C++
    (as first arg, before -x for cross)

--- mkt-int.bat
+++ mkt-int.bat
@@ -1,13 +1,19 @@
 @echo off
-REM $MirOS: src/kern/include/mkt-int.bat,v 1.6 2023/08/25 18:12:55 tg Exp $
+REM $MirOS: src/kern/include/mkt-int.bat,v 1.7 2023/09/13 00:54:10 tg Exp $
 REM
 REM (c) 2023 mirabilos (F) CC0
 REM
 set __VSCMD_ARG_NO_LOGO=1
 set VSCMD_SKIP_SENDTELEMETRY=1
 for /F "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -property installationPath`) do (set "VSINSTALLDIR=%%i\")
+set cxx=
 set x=
 set args=%*
+if not "%1" == "-cxx" goto nocxx
+set cxx=-cxx
+set args=%args:~5%
+shift
+:nocxx
 if not "%1" == "-x" goto nocross
 set x=-x
 set args=%args:~3%
@@ -20,7 +26,7 @@ echo set VSCMD_SKIP_SENDTELEMETRY=1 >>do-cl.bat
 REM echo call "%VSINSTALLDIR%Common7\Tools\VsDevCmd.bat" >>do-cl.bat
 echo call "%VSINSTALLDIR%VC\Auxiliary\Build\vcvarsall.bat" %args% >>do-cl.bat
 echo cl %%* >>do-cl.bat
-wsl /bin/sh mkt-int.sh %x% "$(wslpath "%COMSPEC%")" /c do-cl.bat -DWINVER=0x0602 -D_WIN32_WINNT=0x0602 -DNTDDI_VERSION=NTDDI_WIN8 /Wall -D__STDC_WANT_SECURE_LIB__=1 /wd5045 -DMBSDINT_H_WANT_PTR_IN_SIZET -DMBSDINT_H_WANT_INT32 -DMBSDINT_H_WANT_LRG64 -DMBSDINT_H_WANT_SAFEC /nologo
+wsl /bin/sh mkt-int.sh %cxx% %x% "$(wslpath "%COMSPEC%")" /c do-cl.bat -DWINVER=0x0602 -D_WIN32_WINNT=0x0602 -DNTDDI_VERSION=NTDDI_WIN8 /Wall -D__STDC_WANT_SECURE_LIB__=1 /wd5045 -DMBSDINT_H_WANT_PTR_IN_SIZET -DMBSDINT_H_WANT_INT32 -DMBSDINT_H_WANT_LRG64 -DMBSDINT_H_WANT_SAFEC /nologo
 if errorlevel 1 goto doerror
 del do-cl.bat
 exit /b 0
@@ -32,5 +38,6 @@ call "%VSINSTALLDIR%VC\Auxiliary\Build\vcvarsall.bat" /help
 echo.
 echo Call this with e.g. x86 or amd64 as parameter, see above.
 echo Or call with something like -x x86_arm for cross compilation.
+echo Pass an initial -cxx before everything else to build as C++ not C.
 echo.
 exit /b 1
