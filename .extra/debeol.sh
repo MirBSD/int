e5d555b75b74dea6f2240cc28d6a15f8d12097fc
mirabilos <m@mirbsd.org>
2024-04-02 03:39:43 +0200

    elide -DMBSDINT_H_WANT_LRG64 from C as well for EOL (slink fails)

--- .extra/debeol.sh
+++ .extra/debeol.sh
@@ -56,7 +56,6 @@ sh mkt-int.sh \
     -DMBSDINT_H_WANT_PTR_IN_SIZET \
     -DMBSDINT_H_WANT_SIZET_IN_LONG \
     -DMBSDINT_H_WANT_INT32 \
-    -DMBSDINT_H_WANT_LRG64 \
     -DMBSDINT_H_WANT_SAFEC
 echo ::endgroup::
 if $nocxx; then exit 0; fi
