9aa30a8171dea63b62924aa3d1d2b1de3908bb03
mirabilos <m$(date +%Y)@mirbsd.de>
2025-06-09 21:34:04 +0200

    address qemu/sid packaging changes

--- .extra/cross.sh
+++ .extra/cross.sh
@@ -7,7 +7,7 @@ set -ex
 LC_ALL=C.UTF-8 LANGUAGE=C DEBIAN_FRONTEND=noninteractive
 export LC_ALL DEBIAN_FRONTEND
 unset LANGUAGE
-xarch=$1 xtriplet=$2 xqemu=$3-static
+xarch=$1 xtriplet=$2 xqemu=$3
 echo ::group::Setup $0 on Debian sid/$xarch
 nocxx=false
 cat >dummy.c <<\EOF
@@ -71,7 +71,7 @@ else
 fi
 echo ::endgroup::
 echo ::group::Installing qemu-user
-apt-get install -y qemu-user-static
+apt-get install -y qemu-user
 echo ::endgroup::
 echo ::group::Running under qemu-user
 $xqemu ./.result-c
