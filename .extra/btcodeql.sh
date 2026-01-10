5d0fcbfbfb0879b32f301dbb845cb46c9a1eace8
mirabilos <m$(date +%Y)@mirbsd.de>
2026-01-10 04:03:03 +0000

    extract long "run" lines into scripts

---
+++ .extra/btcodeql.sh
@@ -0,0 +1,1 @@
+: "${CC=cc}${CFLAGS=-O2}"; eval "$(env DEB_BUILD_MAINT_OPTIONS='future=+all qa=+all,-canary optimize=+all,-lto hardening=+all reproducible=+all' dpkg-buildflags --export=sh || :)"; export LDFLAGS; exec sh mkt-int.sh $CC $CPPFLAGS $CFLAGS -Wall -Wextra -DMBSDINT_H_WANT_PTR_IN_SIZET -DMBSDINT_H_WANT_SIZET_IN_LONG -DMBSDINT_H_WANT_INT32 -DMBSDINT_H_WANT_LRG64 -DMBSDINT_H_WANT_SAFEC
